e662adbf585365cfa59ba17646ebccc08b6409c4
Artem Ivlev <artemivlev@yandex.ru>
2022-09-27 17:16:36 +0300

    feat: added i18n lang, refactor structure, added babel loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,41 +1,25 @@
+import React, { Suspense } from 'react';
 import './styles/index.scss';
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
-import {Navbar} from 'widgets/Navbar';
-import {Sidebar} from 'widgets/Sidebar';
-import { Suspense } from 'react';
-import {useTranslation} from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
-
-const Component = () => {
-  const {t, i18n} = useTranslation()
-  const toggle = () => {
-    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
-  }
-  return(
-    <div>
-      <button onClick={toggle}>{t('translate')}</button>
-      {t('test')}
-    </div>
-  )
-}
-
-const App = () => {
-   const { theme } = useTheme();
+function App() {
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-          <Suspense fallback=''>
-          <Component/>
-          <Navbar />
-          <div className="content-page" style={{display:'flex'}}>
-            <Sidebar/>
-            <AppRouter />
-          </div>
-          </Suspense>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
-};
+}
 
 export default App;
